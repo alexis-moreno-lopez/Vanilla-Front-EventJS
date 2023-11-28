f8b894572714cb458fecb623a83a584dbbb80213
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2023-11-28 17:00:11 +0100

    exo_2_eventjs

---
+++ exo_2/exo_2.js
@@ -0,0 +1,20 @@
+
+
+let buttons = document.querySelector(".btn")
+let square = document.querySelector(".square")
+console.log()
+buttons.addEventListener('click', function () {
+
+    square.style.display = "block";
+
+});
+
+
+buttons.addEventListener('click', function () {
+
+
+
+
+
+
+
